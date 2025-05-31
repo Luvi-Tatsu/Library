2422348c26a5483cb30a5c142522661989f817f1
luvi-tatsu <195727939+Luvi-Tatsu@users.noreply.github.com>
2025-05-31 21:08:32 +0800

    tried to create elements 1 by 1

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ const deleteBtn = document.querySelectorAll("button.delete");
 const save = document.querySelector("button.save");
 const updateBook = document.querySelector("button#updateBook");
 const dialog = document.querySelector("dialog.addBook")
+const output = document.querySelector('div.output');
 
-
-deleteBtn.forEach((deleteBtn)=>{
-    deleteBtn.addEventListener("click", ()=>{
-    let display = deleteBtn.parentElement.parentElement;
-    console.log(display);
-    deleteBtn.parentElement.parentElement.remove();
-})
-})
 
 updateBook.addEventListener('click', ()=>{
     dialog.showModal();
@@ -18,16 +11,17 @@ updateBook.addEventListener('click', ()=>{
     console.log(dialog.showModal())
 })
 
+const myLibrary = [];
 
 function Book(title, author,pages,read){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
       }
-
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID()
     this.info = function (){
      return`${this.title} by ${this.author}, ${this.pages}, ${this.read}.`;
     };
@@ -36,3 +30,55 @@ function Book(title, author,pages,read){
 
 const theHobbit = new Book("The Hobbit","J.R.R Tolkien","295 pages","not read yet");
 console.log(theHobbit.info())
+
+function addBookToLibrary(newBook) {
+
+    newBook = new Book("The Hobbit","J.R.R Tolkien","295 pages","not read yet");
+    return myLibrary.push(newBook);  
+  }
+
+  addBookToLibrary('The Hobbit');
+  addBookToLibrary('Lolz');
+  console.log(myLibrary);
+  
+  function display(){
+  let arrayLenght = myLibrary.length;
+  for(let i = 0; i < arrayLenght; i++){
+    displayElements();
+  }
+
+}
+display()
+
+function createElement(type,className){
+    const element = document.createElement(type);
+    element.classList.add(className);
+    return element;
+}
+
+function displayElements(){
+const card = createElement("div",'card');
+    output.appendChild(card);
+const delBtn = createElement("div",'delBtn');
+    card.appendChild(delBtn);
+let del = createElement("button",'delete');
+    del.textContent = "x";
+    delBtn.appendChild(del);
+const content = createElement("div",'content');
+    card.appendChild(content);
+const ulBook = createElement("ul",'ulBook');
+    content.appendChild(ulBook);
+const title = createElement("li",'title');
+    ulBook.appendChild(title);
+    title.textContent = `${myLibrary.title}`;
+const author = createElement("li",'author');
+    ulBook.appendChild(author);
+const pages = createElement("li",'pages');
+    ulBook.appendChild(pages);
+const read = createElement("li",'read');
+    ulBook.appendChild(read);
+}
+
+
+
+
