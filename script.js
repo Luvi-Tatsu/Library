27a071b6d39be1a006e416359601349ca6230b54
luvi-tatsu <195727939+Luvi-Tatsu@users.noreply.github.com>
2025-06-01 15:00:33 +0800

    Editing Delete button

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
  const form = document.querySelector("form");
  const dialog = document.querySelector("dialog.addBook")
  const output = document.querySelector('div.output');
-// const card = document.querySelector('div.card');
-// const delBtn = document.querySelector('div.delBtn');
- const deleteBtn = document.querySelectorAll("button.delete");
-// const content = document.querySelector('div.content');
-// const ulBook = document.querySelector("ul.book");
-// const title = document.querySelector('li.title');
-// const author = document.querySelector('li.author');
-// const pages = document.querySelector('li.pages');
-// const read = document.querySelector('read');
 
 updateBook.addEventListener('click', ()=>{
-    dialog.showModal();
+  dialog.showModal();
 })
 
-const myLibrary = [];
+cancel.addEventListener("click", (e)=>{
+  e.preventDefault();
+  form.reset();
+  dialog.close();
+})
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  addBookToLibrary();
+  showBook()
+  dialog.close();
+  form.reset();
+})
+
+
+let myLibrary = [];
 
 function Book(title, author, pages, read){
     if (!new.target) {
@@ -25,67 +31,73 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    //this.id = crypto.randomUUID();
+    this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary() {
   let title = document.querySelector("input#title").value;
   let author = document.querySelector("input#author").value;
   let pages = document.querySelector("input#page").value;
-  let read = document.querySelector("input#checkbox").value;
+  let read = document.querySelector("input#checkbox").checked;
   let newBook = new Book(title, author,pages, read);
+  newBook
   return myLibrary.push(newBook);
   }
 
-  
-form.addEventListener("submit", (e)=>{
-  e.preventDefault();
-  addBookToLibrary();
-  showBook()
-  dialog.close();
-  form.reset();
-})
-
 function showBook(){
   const lastItem = myLibrary[myLibrary.length-1];
-  const showItems = createLists(lastItem.title,lastItem.author,lastItem.page,lastItem.read);
-  console.log(showItems)
+  const showItems = createLists(lastItem);
+  console.log(myLibrary);
+  console.log(myLibrary.id);
+  return showItems;
 }
 
 function createTable() {
   let card = document.createElement("div");
-  card.classList = 'card';
-  output.append(card);
+  let buttonContainer = document.createElement('div');
+  let deleteBtn = document.createElement('button');
   let content = document.createElement("div");
-    content.classList = 'content';
-   card.append(content);
   let ulBook = document.createElement('ul');
-   ulBook.classList = 'book';
-   content.appendChild(ulBook);
-   return ulBook;
+  card.classList = 'card';
+  buttonContainer.classList = 'delBtn';
+  deleteBtn.classList = 'delete';
+  content.classList = 'content';
+  ulBook.classList = 'book';
+  output.append(card);
+  card.append(buttonContainer);
+  buttonContainer.append(deleteBtn);
+  card.append(content);
+  content.appendChild(ulBook);
+  
+  deleteBtn.textContent = 'x';
+
+  deleteBtn.addEventListener("click", ()=>{
+    let display = deleteBtn.parentElement.parentElement;
+    deleteBtn.parentElement.parentElement.remove();
+    console.log(display);
+    for (let i = 0; i < myLibrary.length; i++){
+      let books = myLibrary[i]
+    }
+})
+  return ulBook;
 }
 
-function createLists(title,author,pages,read){
+function createLists(item){
   let ulBook = createTable();
   let myTitle = document.createElement("li");
   myTitle.classList = 'title';
-  myTitle.textContent = ` ${title}`;
+  myTitle.textContent = `Title: ${item.title}`;
   let myAuthor = document.createElement("li");
   myAuthor.classList = 'author';
-  myAuthor.textContent = ` ${author}`;
+  myAuthor.textContent = `Author: ${item.author}`;
   let myPage = document.createElement("li");
   myPage.classList = 'pages';
-  myPage.textContent = ` ${pages}`;
+  myPage.textContent = `Page: ${item.pages} pages`;
   let myRead = document.createElement("li");
   myRead.classList = 'read';
-  myRead.textContent = ` ${read}`;
+  if(item.read == true){
+  myRead.textContent = "read";
+  }else{myRead.textContent = "not read";}
   ulBook.append(myTitle,myAuthor,myPage,myRead);
+  console.log(item.read)
 }
-
-deleteBtn.forEach((deleteBtn)=>{
-    deleteBtn.addEventListener("click", ()=>{
-    let display = deleteBtn.parentElement.parentElement;
-    console.log(display);
-    deleteBtn.parentElement.parentElement.remove();
-})
-})
